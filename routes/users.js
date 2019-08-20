8e2b7617cb389cde1c6d2b8472c9bbce4fe00d60
ahmed <ahmed.derbala@esprit.tn>
2019-08-20 21:47:29 +0100

    fix index

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+const path = require('path');
+const packagejson = require('../package.json');
+const www = require('../bin/www')
+const appRootPath = require('app-root-path');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.render('index', { title: 'SBP-TECH' });
+  
+  res.status(200).json({app:packagejson.name,version:packagejson.version,NODE_ENV:env,appRootPath,startedAt:www.appStartedAt})
+
 });
 
 module.exports = router;
